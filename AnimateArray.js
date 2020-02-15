cb230f127665b2a1d72fb54b2882b47421dc3748
Jee Zhou <jwhiz113@gmail.com>
2020-02-15 11:31:54 -0600

    first version

--- AnimateArray.js
+++ AnimateArray.js
@@ -1,106 +1,50 @@
-//So now this is the animatearray object:  It takes an array, a canvas and an interval as parameter.
-
-//It has the following state variables:
+//idea:
 /*
-	-this.array (array)
-	-this.canvas
-	-this.id = window.setInterval(
-	-this.displayArray (array)
-	-this.colors (array)
-	-this.action (array)
-	-once made, we are going to run a for loop and and the following:
-		*this.displayArray will be a dedicated copy of the array that is passed in.
-		*this.colors will be initialized to take on the DEFAULT_COLOR.
+pass an array to this class.  then displayArray is then the copy and we'd initiallly draw
+with the original array.  Then we simply draw this array using the original array.
 
-	
-*/ 
-var i =0;
-var j = i;
-AnimateArray2(rectArray, canvas, 100);
+Then in this method, one should have a method called selectionsort that populate the actions
+array, which would use this.array and changes it.  
 
-function AnimateArray2(array, canvas, interval){
-    this.array = array;
-    this.canvas = canvas;
-    this.displayArray=[];
-	this.colorArray = [];
-	this.actions=[];
-    
-    for (var i = 0; i < array.length; i++){
-    	this.displayArray.push(array[i]); //copy the previously generated array
-    	this.colorArray.push(DEFAULT_COLOR);  //Ok so the color array takes on the initial color.
-	}
-	drawArray(this.canvas, this.array, this.colorArray);
-	this.id = window.setInterval(function() {
-		this.step();
-	}, interval);  //change 1000 later
-	
+Then I would simply put the window.setInterval method in a separate function and call it to
+run it!
+
+*/
+
+class AnimateArray{
+    //note the array that AnimateArray takes in should be the randomly generated unsorted array.
+    /*
+    constructor(array, canvas, interval){
+        this.array = array;
+        this.canvas = canvas;
+        this.diplayArray = [];
+        this.colorArray = [];
+        this.actions = [];
+        for (var i = 0; i < this.array.length; i++){  //displayArray is a copy of the original randomly generated array.
+            this.displayArray[i] = this.array[i];
+            this.colorArray.push(DEFAULT_COLOR);
+        }
+        drawArray(this.canvas, this.array, this.colorArray); //drawing the canvas initially when constructor.
+        this.id = window.setInterval(function () {
+            this.step();
+        }, interval);
+    }*/
+    constructor(array, canvas){
+        this.array = array;
+        this.canvas = canvas;
+        this.displayArray=[];
+        this.colorArray=[];
+        this.actions=[];
+        for (var i = 0; i < this.array.length;i++){
+            this.displayArray[i] = this.array[i];
+            this.colorArray.push(DEFAULT_COLOR);
+        }
+        drawArray(this.canvas, this.array, this.colorArray);
+        
+    }
+
+    compare(i, j){
+        this.actions.push(['compare', i, j]);
+        return this.array[j] - this.array[i];  //returns the value of index j - value of index i.
+    }
 }
-//So this works, which means I do get access to the rectArray here.
-var Anime = new AnimateArray2(rectArray, canvas, 12);
-function step(){
-
-	
-	this.colorArray[i] = COMPARE_COLOR;
-	this.colorArray[j] = COMPARE_COLOR;
-	//console.log(colorArray[i]);
-	drawArray(canvas,this.array, this.colorArray);
-	j++;
-	this.colorArray[i]= DEFAULT_COLOR;
-	this.colorArray[j-1]= DEFAULT_COLOR;
-	
-	/*
-	if (this.actions.length == 0){
-		drawArray(this.canvas, this.displayArray, this.colorArray);
-		return;
-	}
-	var action = this.actions.shift();
-	var i = action[1];
-	var j = action[2];
-	if(actions[0] == 'compare'){
-		this.colorArray[i] = COMPARE_COLOR;
-		this.colorArray[j] = COMPARE_COLOR;
-	}else if (action[0] == 'swap'){
-		this.colorArray[i] = SWAP_COLOR;
-		this.colorArray[j] = SWAP_COLOR;
-		var temp = this.displayArray[i];
-		this.displayArray[i] = this.displayArray[j];
-		this.displayArray[j] = temp;
-		
-	}
-	drawArray(this.canvas, this.displayArray, this.colorArray);
-	this.colorArray[i] = DEFAULT_COLOR;
-	this.colorArray[j] = DEFAULT_COLOR;*/
-}
-
-//Idea: I will populate the this.actions array.
-function selectionSort(aa){
-	var n = aa.length;
-	for (var i = 0; i < n-1; i++){
-		var minj = i;
-		for (var j = i; j< n; j++){
-			if (lessThan(j, minj)){
-				minj = j;
-			}
-		}
-		swap(i, minj);
-	}
-}
-
-
-
-function compare(i, j){
-	this.testActions.push(["compare", i, j]);
-	return this.rectArray[i]-this.rectArray[j];
-}
-
-function lessThan(i, j){
-	return this.compare(i,j)<0;  //ith value is smaller than the jth value
-}
-
-function swap(i, j){
-	this.testActions.push(['swap', i, j]);
-	var temp = this.rectArray[i];
-	this.rectArray[i] = this.rectArray[j];
-	this.rectArray[j] = temp;
-}
-
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var DEFAULT_COLOR = '#777';
 var COMPARE_COLOR = '#00F';
 var SWAP_COLOR = '#F00';
@@ -5,8 +6,9 @@ const canvas = document.querySelector("canvas");
 var pen = canvas.getContext('2d');
 
 var rectArray=[]; //just to test it.
-var colorArray=[];
-var actionsArray=[];
+var rectArrayCopy =[];
+
+
 
 //function to generate a random number:
 function randomInteger(low, high){
@@ -18,10 +20,9 @@ function randomInteger(low, high){
 function generateArray(a){
 	for (var i = 0; i<=a; i++){
 		rectArray[i] = randomInteger(30, 600);
+		rectArrayCopy[i] = rectArray[i];
 	}
-	for (var i = 0; i <= a; i++){
-		colorArray[i] = DEFAULT_COLOR;
-	}
+
 }
 
 function drawArray(canvas, array, colors){ //canvas here is important? maybe we'd wanna draw on different canvases...
@@ -37,7 +38,7 @@ function drawArray(canvas, array, colors){ //canvas here is important? maybe we'
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	//next we figure out the spacing: So idea here is the spacing is going to be canvas.width/(3*n)
-	var spacing = canvas.width / (3*array.length);  //So the spacing the going to take up 1/3 of total width
+	var spacing = canvas.width / (3*array.length-1);  //*********** Why is this (3*array.length -1)??
 	var barWidth = spacing*2; //2 of spacing is the width of 1 bar.  Now this words cause array.length*(barWidth + spacing) = canvas.width (work this out!)
 	//now draw rectangles:
 
@@ -50,6 +51,104 @@ function drawArray(canvas, array, colors){ //canvas here is important? maybe we'
 }
 //Now I wish to populate the actions array.
 
+class AnimateArray{
+    //note the array that AnimateArray takes in should be the randomly generated unsorted array.
+    /*
+    constructor(array, canvas, interval){
+        this.array = array;
+        this.canvas = canvas;
+        this.diplayArray = [];
+        this.colorArray = [];
+        this.actions = [];
+        for (var i = 0; i < this.array.length; i++){  //displayArray is a copy of the original randomly generated array.
+            this.displayArray[i] = this.array[i];
+            this.colorArray.push(DEFAULT_COLOR);
+        }
+        drawArray(this.canvas, this.array, this.colorArray); //drawing the canvas initially when constructor.
+        this.id = window.setInterval(function () {
+            this.step();
+        }, interval);
+    }*/
+    constructor(array, canvas){
+        this.array = array;
+        this.canvas = canvas;
+        this.displayArray=[];  //This is now a copy of the generated array
+        this.colorArray=[];  //This is just the color array.  
+        this.actions=[];
+        for (var i = 0; i < this.array.length; i++){
+            this.displayArray[i] = this.array[i];
+            this.colorArray.push(DEFAULT_COLOR);
+        }
+        drawArray(this.canvas, this.array, this.colorArray);
+        
+        
+    }
 
-generateArray(500);//generate an array of 
+    compare(i, j){
+        this.actions.push(['compare', i, j]);
+        return this.array[i] - this.array[j];  //positive means value at index i > value at index j.
+    }
+
+    lessThan(i, j){ //return true or false
+        return this.compare(i, j) <0; //true means value at first index is < value at second index.
+    }
+
+    swap(i, j){  //So here we are simply swapping i and j.  
+        console.log(this.array[i]);
+        this.actions.push(['swap', i, j]);
+        var temp = this.array[i];
+        this.array[i] = this.array[j];
+        this.array[j] = temp;
+    }
+
+    selectionSort(){
+        var n = this.array.length;
+        for (var i = 0; i < n; i++){
+            var minJ = i;
+            for (var j = i; j < n; j++){
+                if (this.lessThan(j, minJ)){  //current j is less than the previous minJ, reset minJ to be current j.
+                    minJ = j;
+                }
+            }
+            this.swap(i, minJ);
+        }
+    }
+
+    step(){
+        //console.log("running");
+        
+        if (this.actions.length == 0){
+            drawArray(this.canvas, this.displayArray, this.colorArray);
+            return;
+        }
+        
+        var action = this.actions.shift();  //action is now an array.  
+        var i = action[1];
+        var j = action[2];
+        if (action[0] === 'compare'){
+            this.colorArray[i] = COMPARE_COLOR;
+            this.colorArray[j] = COMPARE_COLOR;
+            
+        }else if (action[0] === 'swap'){
+            this.colorArray[i]=SWAP_COLOR;
+            this.colorArray[j]=SWAP_COLOR;
+            var temp = this.displayArray[i];
+            this.displayArray[i] = this.displayArray[j];
+            this.displayArray[j] = temp;
+        }
+        drawArray(this.canvas, this.displayArray, this.colorArray);
+        this.colorArray[i] = DEFAULT_COLOR;
+        this.colorArray[j] = DEFAULT_COLOR;
+        
+    }
+}
+
+
+//OK SO FAR SO GOOD!
+generateArray(500);
+var Animate = new AnimateArray(rectArray, canvas);
+Animate.selectionSort();
+window.setInterval(function(){
+    Animate.step();
+}, 1);
 
